4839349266c82fc3c0721812ea570014c68d2ffd
MatthewDZane <43363565+MatthewDZane@users.noreply.github.com>
2022-01-26 01:58:45 -0800

    Uploaded Version 1.0 Files

---
+++ src/Display/Display.java
@@ -0,0 +1,21 @@
+package Display;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Display extends JFrame {
+	
+	public JPanel panel = new JPanel();
+	
+	public Display() {
+		super();
+		
+		panel.
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1000, 1000);
+		//pack();
+	}
+	
+}
---
+++ src/Runner/Main.java
@@ -0,0 +1,9 @@
+package Runner;
+
+import Display.Display;
+
+public class Main {
+	public static void main(String [] args) {
+		new Display();
+	}
+}
